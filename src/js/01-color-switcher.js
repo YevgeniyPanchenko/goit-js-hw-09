bee7ea5f4286dfd8e36e64bde17740991f0130c0
YevgeniyPanchenko <montezheka@gmail.com>
2021-12-05 14:07:50 +0200

    добавляет задание 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+startBtn.addEventListener('click', startSwitcher);
+  startBtn.style.color = 'red';
+  startBtn.style.marginLeft = '20px';
+  startBtn.style.fontSize = '35px';
+  startBtn.style.fontWeight = '400';
+
+stopBtn.addEventListener('click', stopSwitcher);
+  stopBtn.style.color = 'red';
+  stopBtn.style.marginLeft = '20px';
+  stopBtn.style.fontSize = '35px';
+  stopBtn.style.fontWeight = '400';
+
+let color = null;
+let timer = null;
+
+function changeColor() {
+    color = getRandomHexColor();
+    body.style.backgroundColor = `${color}`;
+}
+
+function startSwitcher() {
+    timer = setInterval(changeColor, 1000);
+    startBtn.disabled = true;
+};
+
+function stopSwitcher() {
+    startBtn.disabled = false;
+    clearInterval(timer);
+};
+
+  
